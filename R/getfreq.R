2ba5edb38bb26af8b0e8b1384855b0e8930a39a8
William Lemieux <WLemieux03@users.noreply.github.com>
2021-07-16 16:41:04 -0400

    Minimal commit for functionnal package
    Lacks sufficient annotations and only includes genetic distance calculation

---
+++ R/getfreq.R
@@ -0,0 +1,47 @@
+###########################################
+#'@export
+###########################################
+get.freq <- function(path='.', loci=NULL, exclude = 'ALL', ...){
+  #Some initial parameter setup and autodetection for the populations and loci
+  pops <- setdiff(dir(path) , dir(path, pattern='\\.'))
+  pops <- setdiff(pops, exclude)
+  name <- unlist(strsplit(dir(file.path(path, pops[1]), pattern='.freq$')[1], paste0('_',pops[1],'_|\\.freq')))[1]
+  if(is.null(loci)){
+    for (pop in pops){
+      freq.files <- dir(file.path(path, pop), pattern='.freq$')
+      for (freq.file in freq.files){
+        loci <- c(loci,unlist(strsplit(freq.file, paste0('_',pop,'_|\\.freq')))[2])
+        #lines <- readLines(file(file.path(path, pop, freq.file), open='r'))
+      }
+    }
+    loci <- sort(unique(loci))
+  }
+  
+  #
+  popFreq <- list()
+  for (locus in loci){
+    locusDF <- data.frame()
+    for (pop in pops){
+      filename <- paste(name, pop, paste0(locus, '.freq'),sep='_')
+      temp <- readLines(file(file.path(path, pop, filename), open='r'))
+      alleles <- c()
+      freq <- c()
+      for (t in temp[8:length(r)-1]){
+        all_freq <- unlist(strsplit(t, '\t'))
+        alleles <- c(alleles, all_freq[1])
+        freq <- c(freq, as.numeric(all_freq[2]))
+      }
+      partDF <- data.frame(alleles,freq)
+      names(partDF) <- c('alleles', pop)
+      if('alleles' %in% names(locusDF)){
+        locusDF <- merge(locusDF, partDF, all=T)
+      } else {
+        locusDF <- partDF
+      }
+    }
+    row.names(locusDF) <- locusDF$alleles
+    locusDF[is.na(locusDF)]=0
+    popFreq[[locus]] <- as.matrix(locusDF[,-1])
+  }
+  return(popFreq)
+}
